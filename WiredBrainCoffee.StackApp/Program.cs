47fd7d4ef67078b4ff88d33ad879791a84a8e1aa
Kuldeep-Abjima <119651692+Kuldeep-Abjima@users.noreply.github.com>
2022-12-17 17:37:33 +0530

    Add project files.

---
+++ WiredBrainCoffee.StackApp/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Diagnostics.CodeAnalysis;
+
+namespace WiredBrainCoffee
+{
+    public class Program
+    {
+
+        static void Main(string[] args)
+        {
+            StackDoubles();
+            StackString();
+            Console.ReadLine();
+        }
+
+        private static void StackDoubles()
+        {
+            var stack = new SimpleStack<double>();
+            stack.push(1.2);
+            stack.push(2.8);
+            stack.push(3.0);
+            double sum = 0.0;
+            while (stack.Count > 0)
+            {
+                double item = stack.pop();
+                Console.WriteLine($"Item : {item}");
+                sum = +item;
+            }
+            Console.WriteLine($"Sum : {sum}");
+        }
+        private static void StackString()
+        {
+            var stack = new SimpleStack<string>();
+            stack.push("Wired Coffee");
+            stack.push("Kuldeep");
+            stack.push("Aman");
+            while(stack.Count > 0)
+            {
+                Console.WriteLine(stack.pop() );
+            }
+        }
+    }
+}
---
+++ WiredBrainCoffee.StackApp/SimpleStack.cs
@@ -0,0 +1,53 @@
+﻿using System.Transactions;
+
+namespace WiredBrainCoffee
+{
+    public class SimpleStack<T>
+    {
+        private readonly T[] _item;
+        private int _CurrentIndex = -1;
+
+        public SimpleStack() => _item = new T[10];
+        public int Count => _CurrentIndex+ 1;
+        public void push(T item) => _item[++_CurrentIndex] = item;
+
+        public T pop() => _item[_CurrentIndex--];
+    }
+
+//    public class SimpleStackString
+//    {
+
+//        private readonly string[] _items;
+//        private int _currentIndex = -1;
+//        public SimpleStackString() => _items = new string[10];
+
+
+//        public int count => _currentIndex + 1;
+//        public void Push(string item)
+//        {
+
+//            _items[++_currentIndex] = item;
+
+//        }
+//        public string Pop() => _items[_currentIndex--];
+//    }
+
+
+//}
+//    public class SimpleStackDouble
+//    {
+//        private readonly double[] _items;
+//        private int _currentIndex = -1;
+//        public SimpleStackDouble() => _items = new double[10];
+
+
+//        public int count => _currentIndex + 1;
+//        public void Push(double item)
+//        {
+
+//            _items[++_currentIndex] = item;
+
+//        }
+//        public double Pop() => _items[_currentIndex--];
+//    }
+}
